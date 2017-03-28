c11a16463990eb0152257881c3bdfc4bf407f103
Roilan Hdez Valdes <roylanhv@gmail.com>
2017-03-28 18:17:59 -0300

    Update EBR

--- R/funcoes_roli.R
+++ R/funcoes_roli.R
@@ -240,50 +240,61 @@ EBR <- function(data,
                 coef2 = 0.066,
                 coef3 = 0.22,
                 coef4 = 1.0,
-                adjust = FALSE,
-                method = "non-linear",
-                nsample = 100,
-                max_iter = 10,
-                stats = "rmse"){
+                adjust = FALSE){
    
-    sigma <- 5.67051*10^(-8)
-    
+
     if(func != "-"){
         data$cp <- do.call(func , args = list(data = data)) 
-        start.coefs <- list(coef1 = coef1,coef2=coef2,
-                            coef3 = coef3, coef4= coef4)
+        start.coefs <- c(c1 = coef1, c2 = coef2,ct = coef4, ce = coef5)
     } else { 
         data$cp <- 0
-        start.coefs <- list(coef1 = coef1,coef2=coef2)
+        start.coefs <- c(c1 = coef1, c2 = coef2) #*
     }
     
     if(adjust & method == "non-linear"){
         
-        tmp.nls <- nls( Li/(sigma*Ta^4) ~ 
-                            maxlim( (coef1 + coef2*sqrt(es))*(1+coef3*cp^coef4)  ),
-                        data = data, 
-                        start = start.coefs )
+        if(func != "-"){
+            
+            nls.out <- try(nls( Ofun(Li,Ta) ~ ebr(es,Ta,rh,cp,c1,c2,ct,ce),
+                                data = data,  start = start.coefs ))
+            
+            if(class(nls.out) == "try-error"){
+                
+                resEOfun <- function(par , idata = data) {
+                    idata <- cbind(idata,data.frame(t(par)))
+                    out <- with(idata, Ofun(Li,Ta) - ebr(es,Ta,rh,cp,c1,c2,ct,ce))
+                    out[!is.na(out)]  }
+                
+                nls.out <- 
+                    nls.lm(fn = resEOfun,
+                           par = start.coefs,
+                           idata = data, 
+                           control = nls.lm.control(nprint = 1,maxiter = 1000))
+            }
+            
+        } else {
+            nls.out <- try(nls( Ofun(Li,Ta) ~ ebr(es,Ta,rh,cp,c1,c2),
+                                data = data, 
+                                start = start.coefs ))
+            
+            if(class(nls.out) == "try-error"){
+                
+                resEOfun <- function(par , idata = data) {
+                    idata <- cbind(idata,data.frame(t(par)))
+                    out <- with(idata, Ofun(Li,Ta) - ebr(es,Ta,rh,cp,c1,c2))
+                    out[!is.na(out)]  }
+                
+                nls.out <- 
+                    nls.lm(fn = resEOfun,
+                           par = start.coefs,
+                           idata = data, 
+                           control = nls.lm.control(nprint = 1,maxiter = 1000))
+            }
+        }   
         
-        new.coefs <- coef(tmp.nls) 
-        new.emiss <-
-            with(data = data, 
-                 run_fun(E_fun = EBR,
-                         data = data, 
-                         func = func,
-                         new.coefs = new.coefs) )
+        new.coefs <- coef(nls.out) %>%
+            setNames(paste0("coef",1:length(.)))
         
-        return(list(emiss = new.emiss, coefs = new.coefs))
-        
-    } else if( adjust & method == "montecarlo" ){
-        
-        new.coefs <- 
-            MonteCarlo(data = data,
-                       E_fun = EBR,   ####
-                       func = func,
-                       coefs = unlist(start.coefs),
-                       nsample = nsample,
-                       max_iter = max_iter,
-                       stats = stats)
         
         new.emiss <-
             with(data = data, 
